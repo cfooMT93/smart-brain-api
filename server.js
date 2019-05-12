bc4bdbac07d21fc06deea8853d18b06d21186535
cfooMT93 <minhttran93@gmail.com>
2019-05-12 10:34:20 +0100

    Registering now stores hashed password and email into login db using Transaction knex function and bcrypt synchronous variable. Signin now has a bcrypt compare synchronous function that checks the db for correct credentials--if successful then it will login, else it will notify that you have wrong credentials. Deleted old database variable now that we are using PSQL and KNEX

--- server.js
+++ server.js
@@ -31,33 +31,34 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'Sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
+// NO LONGER NEED THIS, NOW THAT WE HAVE PSQL & KNEX.JS
+// const database = {
+//     users: [
+//         {
+//             id: '123',
+//             name: 'John',
+//             email: 'john@gmail.com',
+//             password: 'cookies',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: '124',
+//             name: 'Sally',
+//             email: 'Sally@gmail.com',
+//             password: 'bananas',
+//             entries: 0,
+//             joined: new Date()
+//         }
+//     ],
+//     login: [
+//         {
+//             id: '987',
+//             hash: '',
+//             email: 'john@gmail.com'
+//         }
+//     ]
+// }
 
 // ROOT route
 app.get('/', (req, res) => {
@@ -66,25 +67,48 @@ app.get('/', (req, res) => {
 
 // SIGNIN
 // test by going to postman: use 'POST' and localhost:3000/signin just like the directory used in here
+// Old Signin Code w/o psql
+// app.post('/signin', (req, res) => {
+//     // res.send('') gives us signin is working but res.json('') gives us the json string "signin is working"
+//     // res.json('signin is working')
+
+//     // compare password hashes 
+//     bcrypt.compare("apples", '$2a$10$hckttkAzmHGJ9PsKyKLZUekjMOJlnXypa/UsGAB0iNNzZqvB.iHxy', function(err, res) {
+//         console.log('first guess', res)
+//     });
+//     bcrypt.compare("veggies", '$2a$10$hckttkAzmHGJ9PsKyKLZUekjMOJlnXypa/UsGAB0iNNzZqvB.iHxy', function(err, res) {
+//         console.log('second guess', res)
+//     });
+
+//     // in order for the code to check with the email object, we must parse it using bodyParser (and using body-parser you need app.use())
+//     if (req.body.email === database.users[0].email &&
+//             req.body.password === database.users[0].password) {
+//             res.json(database.users[0]);
+//     } else {
+//         res.status(400).json('error logging in');
+//     }
+// })
+//
+// New SignIn Code using PSQL & KNEX
 app.post('/signin', (req, res) => {
-    // res.send('') gives us signin is working but res.json('') gives us the json string "signin is working"
-    // res.json('signin is working')
-
-    // compare password hashes 
-    bcrypt.compare("apples", '$2a$10$hckttkAzmHGJ9PsKyKLZUekjMOJlnXypa/UsGAB0iNNzZqvB.iHxy', function(err, res) {
-        console.log('first guess', res)
-    });
-    bcrypt.compare("veggies", '$2a$10$hckttkAzmHGJ9PsKyKLZUekjMOJlnXypa/UsGAB0iNNzZqvB.iHxy', function(err, res) {
-        console.log('second guess', res)
-    });
-
-    // in order for the code to check with the email object, we must parse it using bodyParser (and using body-parser you need app.use())
-    if (req.body.email === database.users[0].email &&
-            req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            // console.log(isValid);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        // console.log(user);
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 })
 
 // REGISTER
@@ -109,15 +133,28 @@ app.post('/signin', (req, res) => {
 // New Register Code - Uses PSQL Database & Knex.js
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-        })
-        .then(user => {
-            res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
         })
         .catch(err => res.status(400).json('unable to register'))
 })
